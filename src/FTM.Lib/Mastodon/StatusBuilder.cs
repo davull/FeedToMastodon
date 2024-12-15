3bdfc80c691dad209ee9623e15f523793e996779
David Ullrich <david.ullrich@ullrich-software.de>
2024-12-15 10:38:23 +0100

    use different formats

--- src/FTM.Lib/Mastodon/StatusBuilder.cs
+++ src/FTM.Lib/Mastodon/StatusBuilder.cs
@@ -58,6 +58,10 @@ public static class StatusBuilder
 
     private static string GetStatus(string title, string summary, Uri? link)
     {
+        // {0}: title
+        // {1}: summary
+        // {2}: link
+
         const string formatWithSummary = """
                                          {0}
 
@@ -70,14 +74,22 @@ public static class StatusBuilder
                                        ---
                                        {2}
                                        """;
+        const string formatWoTitle = """
+                                     {1}
+                                     ---
+                                     {2}
+                                     """;
 
-        var titleAndSummaryAreEqual = ContentComparer.Compare(title, summary) == ContentComparer.CompareResult.Equal;
+        var compare = ContentComparer.Compare(title, summary);
 
-        var useShortFormat = string.IsNullOrWhiteSpace(summary) || titleAndSummaryAreEqual;
-
-        var format = useShortFormat
-            ? formatWoSummary
-            : formatWithSummary;
+        var format = compare switch
+        {
+            // Title contains summary
+            ContentComparer.CompareResult.FirstContainsSecond => formatWoSummary,
+            // Summary contains title
+            ContentComparer.CompareResult.SecondContainsFirst => formatWoTitle,
+            _ => formatWithSummary
+        };
 
         return string.Format(format, title, summary, link);
     }
