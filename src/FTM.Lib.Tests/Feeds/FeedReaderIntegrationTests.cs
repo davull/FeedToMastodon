8f5193c8fd88b1f0020eb03ccab23b80c5e57ae2
David Ullrich <david.ullrich@ullrich-software.de>
2024-12-17 20:41:12 +0100

    Add tests

---
+++ src/FTM.Lib.Tests/Feeds/FeedReaderIntegrationTests.cs
@@ -0,0 +1,81 @@
+﻿using System.Net;
+using FluentAssertions;
+using FTM.Lib.Feeds;
+using NUnit.Framework;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace FTM.Lib.Tests.Feeds;
+
+public class FeedReaderIntegrationTests : TestBase
+{
+    private const string ETag = "\"abc\"";
+
+    private WireMockServer _server = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _server = WireMockServer.Start();
+        SetupServer();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _server.Stop();
+        _server.Dispose();
+    }
+
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase("invalid-etag")]
+    [TestCase("\"valid-etag\"")]
+    public async Task ReadIfChanged_WoMatchingEtag_ShouldReturnFeed(string? etag)
+    {
+        var uri = $"{_server.Url}/feed.xml";
+
+        using var httpClient = new HttpClient();
+        var result = await FeedReader.ReadIfChanged(new Uri(uri), httpClient, etag);
+
+        result.feed.Should().NotBeNull();
+        result.etag.Should().Be(ETag);
+    }
+
+    [Test]
+    public async Task ReadIfChanged_WithMatchingEtag_ShouldNotReturnFeed()
+    {
+        var uri = $"{_server.Url}/feed.xml";
+
+        using var httpClient = new HttpClient();
+        var result = await FeedReader.ReadIfChanged(new Uri(uri), httpClient, ETag);
+
+        result.feed.Should().BeNull();
+        result.etag.Should().Be(ETag);
+    }
+
+    private void SetupServer()
+    {
+        var responseFile = Path.Combine(PathHelper.GetCurrentFileDirectory(),
+            "Responses", "feed.xml");
+
+        _server
+            .Given(Request.Create()
+                .WithPath("/feed.xml")
+                .WithHeader("If-None-Match", ETag)
+                .UsingGet())
+            .RespondWith(Response.Create()
+                .WithStatusCode(HttpStatusCode.NotModified) // 304
+                .WithHeader("ETag", ETag));
+
+        _server
+            .Given(Request.Create()
+                .WithPath("/feed.xml")
+                .UsingGet())
+            .RespondWith(Response.Create()
+                .WithSuccess()
+                .WithHeader("ETag", ETag)
+                .WithBodyFromFile(responseFile));
+    }
+}
