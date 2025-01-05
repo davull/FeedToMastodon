3e2246c0cf303e7ca83219add72b78496a64b1dc
David Ullrich <david.ullrich@ullrich-software.de>
2025-01-05 20:46:05 +0100

    Add LoggerFactoryProvider

---
+++ src/FTM.Lib/LoggerFactoryProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace FTM.Lib;
+
+public static class LoggerFactoryProvider
+{
+    public static ILoggerFactory Create(IConfiguration appConfiguration)
+    {
+        return LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddSimpleConsole(options =>
+                {
+                    options.SingleLine = true;
+                    options.ColorBehavior = LoggerColorBehavior.Enabled;
+                    options.TimestampFormat = "yyyy-MM-dd HH:mm:ss ";
+                })
+                .AddConfiguration(appConfiguration.GetSection("Logging"));
+        });
+    }
+}
