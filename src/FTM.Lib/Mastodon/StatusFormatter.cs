30261030ca3c7a51a4244281ce5e307549be1417
David Ullrich <david.ullrich@ullrich-software.de>
2025-11-27 22:56:34 +0100

    Refactor StatusFormatter

--- src/FTM.Lib/Mastodon/StatusFormatter.cs
+++ src/FTM.Lib/Mastodon/StatusFormatter.cs
@@ -5,7 +5,7 @@ public static class StatusFormatter
     public static string GetStatus(string title, string summary, string tags, Uri? link)
     {
         // TODO: Handle empty link
-        
+
         // {0}: title
         // {1}: summary
         // {2}: tags
@@ -15,98 +15,64 @@ public static class StatusFormatter
         var hasTitle = !string.IsNullOrEmpty(title);
         var hasSummary = !string.IsNullOrEmpty(summary);
 
-        var format = (hasTitle, hasSummary) switch
+        var format = (hasTitle, hasSummary, hasTags) switch
         {
-            // No title and no summary
-            (false, false) => GetFormatWoLinkAndSummary(hasTags),
+            // No title, no summary, no tags
+            (false, false, false) => "{3}",
+            // No title, no summary
+            (false, false, true) => """
+                                    {2}
+                                    ---
+                                    {3}
+                                    """,
             // Title only
-            (true, false) => GetFormatWoSummary(hasTags),
+            (true, false, false) => """
+                                    {0}
+                                    ---
+                                    {3}
+                                    """,
+            // Title and tags
+            (true, false, true) => """
+                                   {0}
+
+                                   {2}
+                                   ---
+                                   {3}
+                                   """,
             // Summary only
-            (false, true) => GetFormatWoTitle(hasTags),
+            (false, true, false) => """
+                                    {1}
+                                    ---
+                                    {3}
+                                    """,
+            // Summary and tags
+            (false, true, true) => """
+                                   {1}
+
+                                   {2}
+                                   ---
+                                   {3}
+                                   """,
             // Title and summary
-            (true, true) => GetFormatWithSummary(hasTags)
+            (true, true, false) => """
+                                   {0}
+
+                                   {1}
+                                   ---
+                                   {3}
+                                   """,
+            // Title, summary, tags
+            (true, true, true) => """
+                                  {0}
+
+                                  {1}
+
+                                  {2}
+                                  ---
+                                  {3}
+                                  """
         };
 
         return string.Format(format, title, summary, tags, link);
-    }
-
-    private static string GetFormatWithSummary(bool hasTags)
-    {
-        if (hasTags)
-        {
-            return """
-                   {0}
-
-                   {1}
-
-                   {2}
-                   ---
-                   {3}
-                   """;
-        }
-
-        return """
-               {0}
-
-               {1}
-               ---
-               {3}
-               """;
-    }
-
-    private static string GetFormatWoSummary(bool hasTags)
-    {
-        if (hasTags)
-        {
-            return """
-                   {0}
-
-                   {2}
-                   ---
-                   {3}
-                   """;
-        }
-
-        return """
-               {0}
-               ---
-               {3}
-               """;
-    }
-
-    private static string GetFormatWoTitle(bool hasTags)
-    {
-        if (hasTags)
-        {
-            return """
-                   {1}
-
-                   {2}
-                   ---
-                   {3}
-                   """;
-        }
-
-        return """
-               {1}
-               ---
-               {3}
-               """;
-    }
-
-    private static string GetFormatWoLinkAndSummary(bool hasTags)
-    {
-        if (hasTags)
-        {
-            return """
-                   {2}
-                   ---
-                   {3}
-                   """;
-        }
-
-        return """
-               {3}
-               """;
     }
 }
